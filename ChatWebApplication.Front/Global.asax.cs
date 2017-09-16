2f55b0f8a697188679127de9a79a480c2808cb19
grik001 <keith.a.gri@gmail.com>
2017-09-16 05:49:28 +0200

    Added bootstrap scripts + bundler

--- ChatWebApplication.Front/Global.asax.cs
+++ ChatWebApplication.Front/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ChatWebApplication.Front.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ChatWebApplication.Front
@@ -13,6 +15,7 @@ namespace ChatWebApplication.Front
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
