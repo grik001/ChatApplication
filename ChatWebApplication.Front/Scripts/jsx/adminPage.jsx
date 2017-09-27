7b2a76916409fab5ba2eea5cb0813d1960b77d75
grik001 <keith.a.gri@gmail.com>
2017-09-28 00:21:13 +0200

    Setup basic React classes for chat functions

--- ChatWebApplication.Front/Scripts/jsx/adminPage.jsx
+++ ChatWebApplication.Front/Scripts/jsx/adminPage.jsx
@@ -1,11 +1,35 @@
 ﻿var ChatHub = React.createClass({
     getInitialState: function () {
-        return { chats: [] };
+        return { chats: [{ id: '123' }] };
+    },
+
+    startChat: function (id) {
+        console.log(this.state.chats);
+        var chats = this.state.chats;
+        chats.push(id);
+        this.setState({ chats: chats });
+    },
+
+    componentWillMount: function () {
+        $.connection.hub.url = "http://localhost:8090/signalr";
+
+        var agent = $.connection.agentHub;
+        var chat = $.connection.chatHub;
+
+        chat.client.startChat = this.startChat;
+
+        $.connection.hub.start().done(function () {
+            agent.server.startAgent($('#displayname').val(), true);
+        });
     },
 
     render: function () {
         return (
-            this.state.Chats.map(game => <Chat key="" />)
+            <div>
+                {
+                    this.state.chats.map(chat => <Chat key={chat.id} />)
+                }
+            </div>
         );
     }
 });
@@ -15,15 +39,24 @@ var Chat = React.createClass({
         return { messages: [], id: "" };
     },
 
+    componentWillMount: function () {
+        var chatID = this.props.id;
+
+
+    },
+
     render: function () {
         return (
             <div>
                 <div>
-                    <MessageList></MessageList>
+                    <MessageList messages={this.state.messages}></MessageList>
                 </div>
                 <div>
-                    <input />
-                    <input />
+                    <input type="text" value="" />
+                </div>
+                <div>
+                    <input type="button" value="Send" />
+                    <input type="button" value="Clear" />
                 </div>
             </div>
         );
@@ -35,7 +68,7 @@ var MessageList = React.createClass({
         return (
             <div className="col-md-12">
                 {
-                    this.state.Messages.map(game => <Message key="" />)
+                    this.props.messages.map(message => <Message key={message.id} text={message.text} username={message.username} />)
                 }
             </div>
         );
@@ -46,8 +79,8 @@ var Message = React.createClass({
     render: function () {
         return (
             <div>
-                <label>{this.props.Username}</label>
-                <label>{this.props.Text}</label>
+                <label>{this.props.username}</label>
+                <label>{this.props.text}</label>
             </div>
         );
     }
